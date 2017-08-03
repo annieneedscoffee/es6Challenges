0728f9ac95cbd9f5d55abd06af7278508d223430
Troy Amelotte <troy.amelotte@gTAef49b1.local>
2017-08-03 13:15:09 -0700

    basic challenges done

---
+++ basicChallenges.js
@@ -0,0 +1,37 @@
+// Pass the array to the function we define below. Pass it without passing values seperately (no addTogether(arr[0], ect)). Hint - Look up "Javascript Spread".
+
+function addTogether(x, y, z){
+  return x+y+z;
+}
+let arr = [4, 8, 12];
+
+
+//For all the challenges below use map/array/filter
+
+// Go through the array below and multiply each value by 3. The result should be [15, 45, 72]
+
+let arr2 = [5, 15, 24]
+
+// Change all the values in the array below from strings to numbers. The result should be [3, 5, 4, 19, 238]
+
+let arr3 = [3, '5', 4, '19', '238'];
+
+// Remove all the values in the array below that are greater than 4. The result should be [3,2]
+
+let arr4 = [3, 8, 5, 2];
+
+// Remove all the strings in the array below and then multiply the remaining values by 2. The result should be [24,8]
+
+let arr5 = [12, "hello", 4, "guys"];
+
+// Multiply all the elements of the array below together into one number. The result should be 8.
+
+let arr6 = [4, 2, 1]
+
+// Remove all even numbers from the array below and return a string containing all remaining elements. The result should be "Here's a even number 2 and here's another one 8"
+
+let arr7 = ["Here's a even number ", 1, 2, " and here's another one ", 8, 9];
+
+// Multiply each value in the array below by itself (square it). Then remove any numbers greater than 100. Finally add all the remaining values together. The result should be 93.
+
+let arr8 = [2, 8, 20, 5, 284];
